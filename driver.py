76fc473fffe6767a56dafdaeacf2254d74833c40
abhimaniyar <abhishek.maniyar@nyu.edu>
2021-08-20 16:32:44 -0400

    driver comments removed

--- driver.py
+++ driver.py
@@ -3,35 +3,6 @@ import power_line_halo
 import cosmo_related
 import input_var
 
-"""
-Some warnings/advice:
-1. As of now, the code calculates the clustering and shot-noise
-spehrically averaged power spectrum for [CII] and CO2-1 lines
-using some recipies (SFR to L_{[CII]} or L_{CO}) available.
-The SFR required is calculated using the best fit parameterization
-of Maniyar 2021 et al. which was developed for a CIB halo model.
-
-2. In the current version of the code, we do not do a full halo-model
-calculation. We just divide the power spectra in th clustering and
-shot noise terms. However, it is pretty straightforward to implement
-the halo model formalism and get the power spectra as 1-halo, 2-halo,
-and shot noise terms.
-
-3. It has to be noted that currently the code is written such that it
-calculates P(k, z) for several redshifts at the same time. Thus, if you
-give only one redshift value as an input, the clustering power spectrum
-will come out with the shape (len(k), 1). On the other hand, shot noise
-is just a number for every redshift and not an array.
-
-4. If you want to run the code several times, or in a MCMC run, or just
-in general want to speed up the calculations, pre-calculate and store
-the halo mass function for a given mass and redshift range and then
-just interpolate for the required mass and redshift ranges. Same
-argument applicable to halo bias and especially the Fourier transform
-of the NFW profile if you want to use the full halo model formalism.
-
-"""
-
 logmass = np.arange(6, 15.005, 0.1)
 mass = 10**logmass1
 # logmass1 = np.arange(10, 14.705, 0.1)  # SAM
